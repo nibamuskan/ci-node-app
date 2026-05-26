9a7cda593c73b0ac0563fb64751d0b1e992bdf70
nibamuskan <nibamuskan992@gmail.com>
2026-05-26 09:51:03 +0530

    Ci node app

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-function add(a ,b){
-    return a+b;
+function add(a, b) 
+{ 
+    return a + b; 
 }
 
-console.log("App running..");
-console.log("Addition Result:",add(2,3));
+console.log("App Running..."); 
+console.log("Addition Result:", add(2,3)); 
 
-module.exports= { add };
+module.exports = add;
